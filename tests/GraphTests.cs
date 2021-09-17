7784648476e2be8887ee645f0f1591e5cbf7187e
Vlad <pikova98@bk.ru>
2021-09-17 18:53:01 +0500

    Added multiple vesitors test

--- tests/GraphTests.cs
+++ tests/GraphTests.cs
@@ -159,5 +159,87 @@ namespace tests
 
             }
         }
+        [Fact]
+        public void Vesit_MultipleVesitors(){
+            var nodes = new Node[9];
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                nodes[i] = new Node(i);
+            }
+            nodes[0].AddChild(nodes[4]);
+            nodes[0].AddChild(nodes[1]);
+
+            nodes[2].AddChild(nodes[1]);
+
+            nodes[3].AddChild(nodes[2]);
+            nodes[3].AddChild(nodes[0]);
+            nodes[3].AddChild(nodes[4]);
+
+            nodes[4].AddChild(nodes[8]);
+            nodes[4].AddChild(nodes[5]);
+
+            nodes[5].AddChild(nodes[2]);
+            nodes[5].AddChild(nodes[6]);
+
+            nodes[6].AddChild(nodes[7]);
+            nodes[6].AddChild(nodes[1]);
+
+            nodes[8].AddChild(nodes[7]);
+            List<int> vesited_nodes1 = new();
+            List<int> vesited_nodes2 = new();
+            var vesitor1 = new ActionVesitor(node =>
+            {
+                vesited_nodes1.Add(node.Id);
+            });
+            var vesitor2 = new ActionVesitor(node =>
+            {
+                vesited_nodes2.Add(node.Id);
+            });
+
+            var graph = new Graph(nodes);
+
+            for (int b = 0; b < 500; b++)
+            {
+                graph.Clear();
+
+                graph.AddVesitor(vesitor1, 5);
+                graph.AddVesitor(vesitor2, 4);
+                vesited_nodes1.Clear();
+                vesited_nodes2.Clear();
+
+                graph.Start();
+                vesited_nodes1.Sort();
+                vesited_nodes2.Sort();
+
+                Assert.Equal(vesited_nodes1, new[] { 5 });
+                Assert.Equal(vesited_nodes2, new[] { 4 });
+
+                vesited_nodes1.Clear();
+                vesited_nodes2.Clear();
+
+
+                graph.Step();
+                vesited_nodes1.Sort();
+                vesited_nodes2.Sort();
+
+                Assert.Equal(vesited_nodes1, new[] { 2,6 });
+                Assert.Equal(vesited_nodes2, new[] { 5,8 });
+
+                vesited_nodes1.Clear();
+                vesited_nodes2.Clear();
+
+
+                graph.Step();
+                vesited_nodes1.Sort();
+                vesited_nodes2.Sort();
+
+                Assert.Equal(vesited_nodes1, new[] { 1, 7 });
+                Assert.Equal(vesited_nodes2, new[] { 2,6,7 });
+
+                vesited_nodes1.Clear();
+                vesited_nodes2.Clear();
+
+            }
+        }
     }
 }
