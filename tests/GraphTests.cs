729d7a1890aa38d2984ced14988724b62497ffde
Vlad <pikova98@bk.ru>
2021-10-06 22:49:04 +0500

    update tests to be more automated. No more handwritten comparisons.

--- tests/GraphTests.cs
+++ tests/GraphTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using GraphSharp;
 using Xunit;
@@ -10,249 +11,47 @@ namespace tests
     public class GraphTests
     {
         [Fact]
-        public void Vesit_ValidateVesitOrder1()
+        public void Vesit_ValidateOrder()
         {
-            var nodes = new Node[9];
-            for (int i = 0; i < nodes.Length; i++)
-            {
-                nodes[i] = new Node(i);
-            }
-            nodes[0].AddChild(nodes[4]);
-            nodes[0].AddChild(nodes[1]);
+            var nodes = NodeGraphFactory.CreateRandomConnected<Node>(1000, 30, 70);
+            const int index = 3;
 
-            nodes[2].AddChild(nodes[1]);
+            var next_gen = new HashSet<NodeBase>();
+            var current_gen = new List<NodeBase>();
+            var buf_gen = new List<NodeBase>();
 
-            nodes[3].AddChild(nodes[2]);
-            nodes[3].AddChild(nodes[0]);
-            nodes[3].AddChild(nodes[4]);
-
-            nodes[4].AddChild(nodes[8]);
-            nodes[4].AddChild(nodes[5]);
-
-            nodes[5].AddChild(nodes[2]);
-            nodes[5].AddChild(nodes[6]);
-
-            nodes[6].AddChild(nodes[7]);
-            nodes[6].AddChild(nodes[1]);
-
-            nodes[8].AddChild(nodes[7]);
-
-            List<int> vesited_nodes = new();
             var vesitor = new ActionVesitor(node =>
             {
-                lock(vesited_nodes)
-                vesited_nodes.Add(node.Id);
+                lock (nodes)
+                {
+                    current_gen.Add(node);
+                    node.Childs.ForEach(n => next_gen.Add(n));
+                }
             });
 
             var graph = new Graph(nodes);
-
-
-            for (int b = 0; b < 500; b++)
-            {
-                graph.Clear();
-
-                graph.AddVesitor(vesitor, 5);
-                graph.AddVesitor(vesitor, 4);
-                graph.AddVesitor(vesitor, 3);
-                vesited_nodes.Clear();
-
-                graph.Start();
-                vesited_nodes.Sort();
-                Assert.Equal(vesited_nodes, new[] { 3, 4, 5 });
-                vesited_nodes.Clear();
-
-                graph.Step();
-                vesited_nodes.Sort();
-
-                Assert.Equal(vesited_nodes, new[] { 0, 2, 4, 5, 6, 8 });
-                vesited_nodes.Clear();
-
-                graph.Step();
-                vesited_nodes.Sort();
-                Assert.Equal(vesited_nodes, new[] { 1, 2, 4, 5, 6, 7, 8 });
-                vesited_nodes.Clear();
-
-                graph.Step();
-                vesited_nodes.Sort();
-                Assert.Equal(vesited_nodes, new[] { 1,2,5,6,7,8 });
-
-            }
-        }
-        [Fact]
-        public void Vesit_ValidateVesitOrder2(){
-            var nodes = new Node[11];
-            for (int i = 0; i < nodes.Length; i++)
-            {
-                nodes[i] = new Node(i);
-            }
-            nodes[0].AddChild(nodes[5]);
-            nodes[0].AddChild(nodes[1]);
-
-            nodes[1].AddChild(nodes[4]);
-            nodes[1].AddChild(nodes[6]);
-            nodes[1].AddChild(nodes[7]);
-
-            nodes[2].AddChild(nodes[1]);
-            nodes[2].AddChild(nodes[0]);
-            nodes[2].AddChild(nodes[9]);
-            nodes[2].AddChild(nodes[10]);
-
-            nodes[3].AddChild(nodes[2]);
-            nodes[3].AddChild(nodes[4]);
-
-            nodes[5].AddChild(nodes[4]);
-            nodes[5].AddChild(nodes[3]);
-
-            nodes[6].AddChild(nodes[8]);
-            nodes[6].AddChild(nodes[10]);
-
-            nodes[7].AddChild(nodes[6]);
-
-            nodes[8].AddChild(nodes[7]);
-            nodes[8].AddChild(nodes[9]);
-
-            nodes[9].AddChild(nodes[8]);
-
-            nodes[10].AddChild(nodes[9]);
-            nodes[10].AddChild(nodes[6]);
-
-            List<int> vesited_nodes = new();
-            var vesitor = new ActionVesitor(node =>
-            {
-                lock(vesited_nodes)
-                vesited_nodes.Add(node.Id);
-            });
-
-            var graph = new Graph(nodes);
-
-
-            for (int b = 0; b < 500; b++)
-            {
-                graph.Clear();
-
-                graph.AddVesitor(vesitor, 0);
-                graph.AddVesitor(vesitor, 5);
-                graph.AddVesitor(vesitor, 10);
-                graph.AddVesitor(vesitor, 2);
-
-                vesited_nodes.Clear();
-
-                graph.Start();
-                vesited_nodes.Sort();
-                Assert.Equal(vesited_nodes, new[] { 0,2,5,10 });
-                vesited_nodes.Clear();
-
-                graph.Step();
-                vesited_nodes.Sort();
-
-                Assert.Equal(vesited_nodes, new[] { 0,1,3,4,5,6,9,10 });
-                vesited_nodes.Clear();
-
-                graph.Step();
-                vesited_nodes.Sort();
-                Assert.Equal(vesited_nodes, new[] { 1,2,3,4,5,6,7,8,9,10 });
-                vesited_nodes.Clear();
-
-                graph.Step();
-                vesited_nodes.Sort();
-                Assert.Equal(vesited_nodes, new[] { 0,1,2,3,4,6,7,8,9,10});
-                vesited_nodes.Clear();
-
-            }
-        }
-        [Fact]
-        public void Vesit_MultipleVesitors(){
-            var nodes = new Node[9];
-            for (int i = 0; i < nodes.Length; i++)
-            {
-                nodes[i] = new Node(i);
-            }
-            nodes[0].AddChild(nodes[4]);
-            nodes[0].AddChild(nodes[1]);
-
-            nodes[2].AddChild(nodes[1]);
-
-            nodes[3].AddChild(nodes[2]);
-            nodes[3].AddChild(nodes[0]);
-            nodes[3].AddChild(nodes[4]);
-
-            nodes[4].AddChild(nodes[8]);
-            nodes[4].AddChild(nodes[5]);
-
-            nodes[5].AddChild(nodes[2]);
-            nodes[5].AddChild(nodes[6]);
-
-            nodes[6].AddChild(nodes[7]);
-            nodes[6].AddChild(nodes[1]);
-
-            nodes[8].AddChild(nodes[7]);
-            List<int> vesited_nodes1 = new();
-            List<int> vesited_nodes2 = new();
-            var vesitor1 = new ActionVesitor(node =>
-            {
-                vesited_nodes1.Add(node.Id);
-            });
-            var vesitor2 = new ActionVesitor(node =>
-            {
-                vesited_nodes2.Add(node.Id);
-            });
-
-            var graph = new Graph(nodes);
-
-            for (int b = 0; b < 500; b++)
-            {
-                graph.Clear();
-
-                graph.AddVesitor(vesitor1, 5);
-                graph.AddVesitor(vesitor2, 4);
-                vesited_nodes1.Clear();
-                vesited_nodes2.Clear();
-
-                graph.Start();
-                vesited_nodes1.Sort();
-                vesited_nodes2.Sort();
-
-                Assert.Equal(vesited_nodes1, new[] { 5 });
-                Assert.Equal(vesited_nodes2, new[] { 4 });
-
-                vesited_nodes1.Clear();
-                vesited_nodes2.Clear();
-
-
-                graph.Step();
-                vesited_nodes1.Sort();
-                vesited_nodes2.Sort();
-
-                Assert.Equal(vesited_nodes1, new[] { 2,6 });
-                Assert.Equal(vesited_nodes2, new[] { 5,8 });
-
-                vesited_nodes1.Clear();
-                vesited_nodes2.Clear();
-
-
-                graph.Step();
-                vesited_nodes1.Sort();
-                vesited_nodes2.Sort();
-
-                Assert.Equal(vesited_nodes1, new[] { 1, 7 });
-                Assert.Equal(vesited_nodes2, new[] { 2,6,7 });
-
-                vesited_nodes1.Clear();
-                vesited_nodes2.Clear();
-
-            }
-        }
-        [Theory]
-        [InlineData(9000,100,1000,50,500)]
-        public void Run_PerfomanceMeasure(int count_of_nodes, int min_count_of_childs, int max_count_of_childs,int delay,int count_of_steps){
-            var nodes = NodeGraphFactory.CreateRandomConnected<Node>(count_of_nodes,min_count_of_childs,max_count_of_childs);
-            var graph = new Graph(nodes);
-            var vesitor = new ActionVesitor(node=>{
-                Task.Delay(delay).Wait();
-            });
+            graph.AddVesitor(vesitor, index);
             graph.Start();
-            for(int i = 0;i<count_of_steps;i++){
+            //current_get here is node3
+            //next_gen is what will be current_gen in next iteration
+            
+            //save next_gen
+            buf_gen = next_gen.ToList();
+            buf_gen.Sort((v1,v2)=>v1.Id-v2.Id);
+            
+            next_gen.Clear();
+            current_gen.Clear();
+
+            for(int i = 0;i<50;i++){
                 graph.Step();
+                current_gen.Sort((v1,v2)=>v1.Id-v2.Id);
+                Assert.Equal(buf_gen,current_gen);
+
+                buf_gen = next_gen.ToList();
+                buf_gen.Sort((v1,v2)=>v1.Id-v2.Id);
+
+                next_gen.Clear();
+                current_gen.Clear();
             }
         }
     }
