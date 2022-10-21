857b6519760d2fb44e7672e1c982064b201e19b1
Vlad <pikova98@bk.ru>
2022-10-21 20:54:18 +0500

    Moved impl of FindSpanningTreeDegree2OnNodes to more abstract function

--- GraphSharp/GraphStructures/GraphOperations/FindSpanningForest.cs
+++ GraphSharp/GraphStructures/GraphOperations/FindSpanningForest.cs
@@ -24,9 +24,9 @@ where TEdge : IEdge
         forest.Dispose();
         return result;
     }
-    KruskalForest<TEdge> FindForestDegree1(IGraph<TNode, TEdge> graph)
+    KruskalForest<TEdge> FindForestDegree1(IGraph<TNode, TEdge> graph, Func<TEdge,float> getWeight)
     {
-        var edges = graph.Edges.OrderBy(x => x.Weight);
+        var edges = graph.Edges.OrderBy(x => getWeight(x));
         return graph.Do.KruskalAlgorithm(edges, x => 1);
     }
     /// <summary>
@@ -37,14 +37,30 @@ where TEdge : IEdge
     public IList<TEdge> FindSpanningTreeDegree2OnNodes(Func<TEdge, float>? getWeight = null)
     {
         getWeight ??= x=>x.Weight;
+        return FindSpanningTreeDegree2OnNodes(getWeight,graph=>graph.Do.DelaunayTriangulation()).tree;
+    }
+    /// <summary>
+    /// Constructs a spanning tree degree 2 on nodes only by repeatedly applying delaunay triangulation
+    /// and merging results. <br/>
+    /// Resulting tree is about the same length as one formed by complete graph (maybe 2-5 % higher)
+    /// but a lot faster(in asymptote especially)
+    /// and so it can be used to build a TSP by cheapest link approach or anything that
+    /// requires tree degree 2 that connects all nodes.
+    /// </summary>
+    /// <param name="tree">Resulting tree in edges</param>
+    /// <param name="getWeight">Function to take weight from edge</param>
+    /// <param name="doDelaunayTriangulation">Function to do delaunay triangulation</param>
+    /// <returns>Tree as edges list and tree ends as nodes array. Node is end if it's degree = 1</returns>
+    (IList<TEdge> tree,TNode[] ends) FindSpanningTreeDegree2OnNodes(Func<TEdge, float> getWeight, Action<IGraph<TNode,TEdge>> doDelaunayTriangulation)
+    {
         var graph = StructureBase;
         var clone = graph.CloneJustConfiguration();
         clone.SetSources(graph.Nodes);
-        clone.Do.DelaunayTriangulation();
+        doDelaunayTriangulation(clone);
         var startEdges = clone.Do.FindSpanningForestKruskal(getWeight: getWeight,maxDegree: n => 2);
         clone.SetSources(edges: startEdges);
         TNode[] ends;
-        for (int i = 0; i < 4; i++)
+        while(true)
         {
             using var components = clone.Do.FindComponents();
             var nodesDegreeBelow2 = clone.Nodes.Where(n => clone.Edges.Degree(n.Id) < 2).ToList();
@@ -55,15 +71,16 @@ where TEdge : IEdge
                 ends = nodesDegreeBelow2.ToArray();
                 break;
             }
-            tmpGraph.Do.DelaunayTriangulation();
-            foreach (var e in tmpGraph.Edges.ToList())
+            doDelaunayTriangulation(tmpGraph);
+            foreach(var n in tmpGraph.Nodes)
+            foreach (var e in tmpGraph.Edges.OutEdges(n.Id))
             {
                 if (components.InSameComponent(e.SourceId, e.TargetId))
                 {
                     tmpGraph.Edges.Remove(e.SourceId, e.TargetId);
                 }
             }
-            using var forest = FindForestDegree1(tmpGraph);
+            using var forest = FindForestDegree1(tmpGraph,getWeight);
             var setFinder = components.SetFinder;
             foreach (var e in forest.Forest.OrderBy(x =>getWeight(x)))
             {
@@ -72,6 +89,6 @@ where TEdge : IEdge
                 setFinder.UnionSet(e.SourceId, e.TargetId);
             }
         }
-        return clone.Edges.ToList();
+        return (clone.Edges.ToList(),ends);
     }
 }
