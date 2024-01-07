8c4dc7c1b2e04d4fa4e8c7ee5a059ecb279888e0
Vlad <pikova98@bk.ru>
2024-01-08 03:56:11 +0500

    added tolerance to NodeEmeddings

--- GraphSharp/Algorithms/GraphOperations/NodeEmbredding.cs
+++ GraphSharp/Algorithms/GraphOperations/NodeEmbredding.cs
@@ -8,11 +8,16 @@ public partial class ImmutableGraphOperation<TNode, TEdge>
 where TNode : INode
 where TEdge : IEdge
 {
+    
     /// <summary>
     /// Creates nodes embeddings that represents node unique position relative to all other nodes and edges in a graph.<br/>
     /// Can be used to determine if two graphs are isomorphic.
     /// </summary>
-    public IDictionary<int, double[]> NodesEmbedding()
+    /// <param name="tolerance">
+    /// Tolerance to node embeddings. Varying these greatly changes execution time
+    /// </param>
+    /// <returns>Embeddings nodeId -> vector</returns>
+    public IDictionary<int, double[]> NodesEmbedding(double tolerance = 0.01)
     {
         //here we use different structural information about nodes to give them unique embeddings.
         //This setup may differ, here I use pagerank, HITS and local clustering coefficients
@@ -22,14 +27,14 @@ where TEdge : IEdge
         IDictionary<int, double> auth = new Dictionary<int, double>();
         IDictionary<int, double> hub = new Dictionary<int, double>();
 
-        pagerank = StructureBase.Do.PageRank(0.9).Ranks;
+        pagerank = StructureBase.Do.PageRank(0.9,tolerance).Ranks;
         using var c1 = StructureBase.Do.FindLocalClusteringCoefficients();
         var clustering = c1.ToArray();
 
         var mostImportantNodes = pagerank.Keys.Zip(pagerank.Keys.Select(k => pagerank[k])).OrderBy(v => -v.Second).ToArray();
 
         var rootSet = mostImportantNodes.Take(Math.Max(StructureBase.Nodes.Count() / 4, 1)).Select(i => i.First).ToArray();
-        var c = StructureBase.Do.HITS(rootSet, 0.01);
+        var c = StructureBase.Do.HITS(rootSet, tolerance);
         auth = c.AuthScores;
         hub = c.HubScores;
 
