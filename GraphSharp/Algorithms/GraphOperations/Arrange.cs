61230943e9358acc5c4e9a12bec298767fdc790f
Vlad <pikova98@bk.ru>
2024-01-25 22:47:46 +0500

    graph arrange method overload

--- GraphSharp/Algorithms/GraphOperations/Arrange.cs
+++ GraphSharp/Algorithms/GraphOperations/Arrange.cs
@@ -22,5 +22,17 @@ where TEdge : IEdge
         while(p.ComputeStep()>theta) ;
         return p;
     }
+    ///<summary>
+    /// Arranges graph and returns graph arrangement
+    ///</summary>
+    /// <param name="iterations">How many iterations to run</param>
+    /// <param name="closestCount">Count of closest to given node elements to compute repulsion from. Let it be -1 so all nodes will be used to compute repulsion</param>
+    /// <param name="getWeight">How to measure edge weights. By default will use distance between edge endpoints.</param>
+    public GraphArrange<TNode, TEdge> Arrange(int iterations, int closestCount = -1, Func<TEdge,float>? getWeight = null)
+    {
+        var p = new GraphArrange<TNode,TEdge>(StructureBase);
+        while(iterations-->0) p.ComputeStep();
+        return p;
+    }
 
 }
